85227c82df810261fd0688401c04c987c78032ab
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2025-07-04 21:44:21 +0530

    Update index.tsx

--- nirmalya/doklink/client/app/index.tsx
+++ nirmalya/doklink/client/app/index.tsx
@@ -115,21 +115,3 @@ export default function App() {
   // If not authenticated, show StartingScreen
   return <StartingScreen onLogin={handleLogin} onSignUp={handleSignUp} />;
 }
-
-
-// ok i want to break down the codes into components. i have created a components folder outside the app folder and an utils folder. inside the components folder there will be a common folder and also folders representing each page/ For your reference i have created a login folder for Login.tsx/ similarly you will create signup for SignUp.tsx and so on.
-
-// Let me also tell you how the navigation works so that you do not mess up the navigation.
-
-// index.tsx to StartingScreen.tsx
-// StartingScreen.tsx to 3 pages (Login.tsx, AadharVerification.tsx, AboutUs.tsx)
-// Login.tsx to Home.tsx Optionally can travel to AafharVerification.tsx by clicking the signup option
-// AadharVerification.tsx to SignUp.tsx optionally can travel to Login,tsx by clicking the login option.
-// SignUp.tsx can travel to four pages(TermsAndConditions.tsx, DataCollectionConsentForm.tsx, PrivacyPolicy.tsx and Home.tsx) Optionally can travel Login.tsx by clicking the login option. 
-// index.tsx can also travel directly to Home.tsx under certain conditions. 
-
-// Any page with a back button can travel to the previous page.
-
-// So first analuze the entire project and only then  get to work
-
-// Any common components should be stored inside the components/common
